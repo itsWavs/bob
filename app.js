0d5c9c0663bd53aba7e144a617bf9c0c4e3f626b
Dominik Ulewicz <ulewicz95@gmail.com>
2024-04-13 09:13:44 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const inputText = document.getElementById("input_string");
+const outputField = document.getElementById("output");
+const bobifyButton = document.getElementById("bobifyButton");
+const clipboardButton = document.getElementById("clipboardButton");
+const slider = document.getElementById("slider");
+const temperatureDisplay = document.getElementById("temperatureDisplay");
+
+bobifyButton.addEventListener("click", handleBobify);
+clipboardButton.addEventListener("click", handleClipboard);
+slider.addEventListener("input", () => {
+  displayTemperature();
+  handleBobify();
+});
+
+// temperatureDisplay.innerText = slider.value;
+displayTemperature();
+
+function displayTemperature() {
+  temperatureDisplay.innerText = slider.value + "%";
+}
+function bobify(inputString) {
+  outputArray = [];
+  loweredInput = inputString.toLowerCase();
+  for (character of loweredInput) {
+    isUpper = Math.random() < slider.value / 100;
+    outputArray.push(
+      isUpper ? character.toUpperCase() : character.toLowerCase()
+    );
+  }
+  return outputArray.join("");
+}
+
+function handleBobify() {
+  const userString = inputText.value;
+  outputField.innerText = bobify(userString);
+}
+
+function handleClipboard() {
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(outputField.innerText);
+}
